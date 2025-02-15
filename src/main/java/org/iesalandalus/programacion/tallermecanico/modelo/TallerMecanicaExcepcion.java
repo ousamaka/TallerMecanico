7ce5a9ea57a4d9035927e595aa0287366195e59b
Ousama Kassimi <ousamakassimi02@gmail.com>
2025-02-15 13:28:34 +0100

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicaExcepcion.java
+++
@@ -1,7 +0,0 @@
-package org.iesalandalus.programacion.tallermecanico.modelo;
-
-public class TallerMecanicaExcepcion extends Exception {
-    public TallerMecanicaExcepcion(String message) {
-        super(message);
-    }
-}
