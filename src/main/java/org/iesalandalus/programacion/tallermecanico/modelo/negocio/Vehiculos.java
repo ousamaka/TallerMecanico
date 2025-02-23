1c18a3ff07683e6658c654ad15174e72d95076aa
Ousama Kassimi <ousamakassimi02@gmail.com>
2025-02-23 22:26:35 +0100

    Terminado clase vehiculos y  Vehiculos test

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -1,4 +1,45 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Vehiculos {
+
+    private final List<Vehiculo> vehiculos;
+
+    public Vehiculos() {
+        vehiculos = new ArrayList<>();
+    }
+
+    public List<Vehiculo> get() {
+        return new ArrayList<>(vehiculos); // Devuelve copia defensiva
+    }
+
+    public void insertar(Vehiculo vehiculo) throws TallerMecanicoExcepcion{
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        if (vehiculos.contains(vehiculo)) {
+            throw new TallerMecanicoExcepcion("Ya existe un vehículo con esa matrícula.");
+        }
+        vehiculos.add(vehiculo);
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        Vehiculo vehiculoEncontrado = null;
+        if (vehiculos.contains(vehiculo)) {
+            vehiculoEncontrado = vehiculo;
+        }
+        return vehiculoEncontrado;
+    }
+
+    public void borrar(Vehiculo vehiculo) throws TallerMecanicoExcepcion {
+        Objects.requireNonNull(vehiculo,"No se puede borrar un vehículo nulo.");
+        if (!vehiculos.remove(vehiculo)) {
+            throw new TallerMecanicoExcepcion("No existe ningún vehículo con esa matrícula.");
+        }
+    }
 }
